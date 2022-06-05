3d1c0c412ce6fee6a702aa3853194ebf8e2d9fba
casiimir <cp.ciancimino@gmail.com>
2022-06-05 14:37:39 +0200

    feat: add default layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import MessageList from "./components/MessageList.vue";
+import DefaultLayout from "./layouts/DefaultLayout.vue";
 </script>
 
 <template>
-  <MessageList />
+  <DefaultLayout navbarTitle="Document list">
+    <MessageList />
+  </DefaultLayout>
 </template>
 
-<style></style>
+<style lang="scss">
+@import "./styles/global.scss";
+@import "./styles/variables.scss";
+
+body {
+  background: $bg-dark-grey-color;
+  font-family: "VT323", monospace;
+}
+</style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const { navbarTitle } = defineProps(["navbarTitle"]);
+const localDate = ref(new Date().toISOString().split("T")[0]);
+</script>
+
+<template>
+  <nav class="navbar">
+    <p>{{ navbarTitle }}</p>
+    <p>{{ localDate }}</p>
+  </nav>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+@import "../styles/mixins";
+
+.navbar {
+  @include display-flex(row, space-between);
+  margin: 0 20% 80px;
+  padding: 5px 10px;
+  background: $primary-green-color;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-shadow: 0 0 1px $bg-dark-grey-color;
+  box-shadow: 0 0 3px $primary-green-color;
+
+  p {
+    margin: 0;
+  }
+}
+
+.wrapper {
+  margin: 20px 20%;
+}
+</style>
