786450518a578418d865505d9542a290d918e6dd
casiimir <cp.ciancimino@gmail.com>
2022-06-05 16:19:12 +0200

    feat: created <Note /> component and handle the on click event

---
+++ src/components/Note.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+
+const { note } = defineProps(["note"]);
+const router = useRouter();
+
+const onCardClick = () => router.push(`/reader/${note.id - 1}`);
+</script>
+
+<template>
+  <div class="note" @click="onCardClick">
+    <div class="infos">
+      <p>{{ note.time }}</p>
+      <p>LENGHT: {{ note.content.length }} CHARS.</p>
+    </div>
+    <p class="title">{{ note.title.toUpperCase() }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "../styles/mixins.scss";
+@import "../styles/variables.scss";
+
+.note {
+  padding: 14px 2px 0;
+  min-width: 240px;
+  border: 1px solid $primary-green-color;
+  color: $primary-green-color;
+
+  text-shadow: 0 0 1px $primary-green-color;
+  box-shadow: 0 0 3px $primary-green-color;
+  cursor: pointer;
+  transition: all 0.35s;
+
+  .infos {
+    p {
+      margin: 5px 0;
+      padding: 0 0 0 25px;
+    }
+  }
+
+  .title {
+    margin: 5px 0;
+    padding: 5px;
+    background: $primary-green-color;
+    color: $bg-dark-grey-color;
+    font-weight: bold;
+    text-shadow: 0 0 1px $bg-dark-grey-color;
+    transition: all 0.7s;
+  }
+
+  &:hover {
+    background: $primary-green-color;
+    color: $bg-dark-grey-color;
+
+    .title {
+      background: $bg-dark-grey-color;
+      color: $primary-green-color;
+    }
+  }
+}
+</style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Note from "./Note.vue";
 import noteListMock from "../assets/mock.json";
 import { ref } from "vue";
 
@@ -7,13 +8,7 @@ const noteList = noteListMock["note-list"];
 
 <template>
   <div class="note-wrapper">
-    <div v-for="note in noteList" :key="note.id" class="note">
-      <div class="infos">
-        <p>{{ note.time }}</p>
-        <p>LENGHT: {{ note.content.length }} CHARS.</p>
-      </div>
-      <p class="title">{{ note.title.toUpperCase() }}</p>
-    </div>
+    <Note v-for="note in noteList" :key="note.id" :note="note" />
   </div>
 </template>
 
@@ -23,43 +18,5 @@ const noteList = noteListMock["note-list"];
 
 .note-wrapper {
   @include display-flex(row, center, flex-start, 20px, wrap);
-}
-.note {
-  padding: 14px 2px 0;
-  min-width: 240px;
-  border: 1px solid $primary-green-color;
-  color: $primary-green-color;
-
-  text-shadow: 0 0 1px $primary-green-color;
-  box-shadow: 0 0 3px $primary-green-color;
-  cursor: pointer;
-  transition: all 0.35s;
-
-  .infos {
-    p {
-      margin: 5px 0;
-      padding: 0 0 0 25px;
-    }
-  }
-
-  .title {
-    margin: 5px 0;
-    padding: 5px;
-    background: $primary-green-color;
-    color: $bg-dark-grey-color;
-    font-weight: bold;
-    text-shadow: 0 0 1px $bg-dark-grey-color;
-    transition: all 0.7s;
-  }
-
-  &:hover {
-    background: $primary-green-color;
-    color: $bg-dark-grey-color;
-
-    .title {
-      background: $bg-dark-grey-color;
-      color: $primary-green-color;
-    }
-  }
 }
 </style>
