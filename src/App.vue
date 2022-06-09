1132704855d7c2b4429287f832945ee4a2325580
casiimir <cp.ciancimino@gmail.com>
2022-06-10 00:40:21 +0200

    style: body vintage effect

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,17 @@ onMounted(() => {
 }
 
 body {
-  background: $bg-dark-grey-color;
+  height: 100vh;
+  background: rgb(32, 32, 32);
+  background: radial-gradient(
+    circle,
+    rgba(32, 32, 32, 1) 0%,
+    rgba(19, 19, 19, 1) 74%
+  );
   font-family: "VT323", monospace;
+  filter: blur(0.1px);
+
+  overflow: hidden;
+  transform: translateY(5px) rotate(0.05deg) skew(-0.1deg, 0.1deg);
 }
 </style>
